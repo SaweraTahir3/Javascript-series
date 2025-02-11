20ad83d3fc7829c1928c42aac8e0a1e35f72ea5a
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-02-11 11:42:31 +0500

    Completed

--- javascript-series/Functions.js
+++ javascript-series/Functions.js
@@ -104,5 +104,73 @@ if (true) {
         console.log('please inter true condition');
         }
 
+// Arrow Functions
+const ArrowFunction = () => {
+    console.log('hello arrowFunction');
+}
+ArrowFunction()
+
+//parametar ki sath arrow function
+const ArrowFunction2 = (num, num2) => {
+    return num + num2
+}
+console.log(ArrowFunction2(10, 15))
+
+//single line arrow function
+const singleLine = (add, add2) => (add + add2)
+console.log(singleLine("javascript", " series "));
+
+// arrow function me object kasie likhi 
+const objInFunction = () => ({
+    userName: "object in function"
+})
+console.log(objInFunction())
+
+// THIS KEY WORD USE IN OBJECT
+const grocreyList = {
+    fruit: "mango",
+    vegtable: "ladyfingar",
+    biscutis: "Mari",
+    milk: "2 kilo garam ",
+    fastFood: " MIcroni",
+
+    myGrocreyList: function () {
+        console.log(`${this.milk} milk pack`);
+        console.log(this); // THIS  key word ni  pura  current object print kar ki deya he.  
+    }},
+
+grocreyList.myGrocreyList() // current condition in milk  print this function
+grocreyList.milk = "1 Kilo garam more " // now milk value change this line
+grocreyList.myGrocreyList()// again print the current milk value this  line  
+
+// this ka use simple function and arrow function me
+function user() {
+    console.log(this);
+    }
+// console.log(user());//  object Global   araha he node.js me  or undefined  answer 
+
+function user2() {
+    let username = "kiya this  exess kari ga  username ko ?"
+    console.log(username);
+    console.log(this.username);
+}
+console.log(user2());// undefined  answer
+
+const  arrowUser =() =>{
+    let username = "arrow function  (this) key exess kari ga  username ko ?"
+    console.log(username);//print
+    console.log(this.username);//undefined
+    console.log(this);//empty {}
+    
+}
+console.log(arrowUser());//undefined answer
+console.log(this); //empty
+
+// Note : (THIS)) key words current  value ko exess karta he  node.js me
+// globar scope and block scope ka dehan rakhna zaroori he 
+// node.js me This key word ka work  current condition ko exess karta he objects me
+// function and arrow function me this key word node.js me empty brakets  and undefined deta he 
+// this key word Brower me alag tareeqi si  work karta he yani  global object  brower me deta he 
+
  // --------------------------------end -------------------------------------------
 
