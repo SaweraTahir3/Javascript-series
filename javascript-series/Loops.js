b79720dd354537a3cb35b757f6ec4d04dff4ccea
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-02-17 10:36:14 +0500

    Working This Topic

--- javascript-series/Loops.js
+++ javascript-series/Loops.js
@@ -1,1 +1,83 @@
+// for loop
 
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("5 is the best number");
+//     }
+//     console.log(element);
+// }
+
+
+//  Nested for loop  example  
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`outer loop value ${i}`);
+
+    for (let j = 0; j <= 10; j++) {
+        console.log(`inner loop value ${j} and inner loop ${i}`);
+    }
+}
+// parent loop  phele ek dafa chali ga then wo child me chala jai ga
+//  child pura 10 tak print hoga then wapis parent me jai ga srif  1 dafa
+// chali ga phir si child me ajai ga child pura 10 dafa chali ga .
+// as tarha hota rahi ga jab tak parent ki condition false ni hoge tab tak
+//  parent apni sath child ko bar bar print kari ga 
+
+// table 
+for (let i = 1; i <= 5; i++) {
+    console.log(`outer loop value ${i}`);
+    
+   for (let t = 1; t <= 10; t++) { 
+    console.log( i + '*' + t + "=" + i * t);
+   }
+}
+//array in loop
+let myArray = ["apple" , "mango" ,"orange"]
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    console.log(element);
+}
+
+// break and continue
+
+for (let index = 0; index <=20 ; index++) {
+    if (index == 5) {
+        console.log(`Detected 5 `);
+        break
+    }
+    console.log(`value of i is ${index}`);
+}
+
+for (let index = 1; index <=10 ; index++) {
+    if (index == 6) {
+        console.log(`Detected 6 `);
+        continue
+    }
+    console.log(`value of i is ${index}`);
+}
+
+//while loop
+let index = 0 ;
+while (index <= 20) {
+console.log(`value of index  is ${index}`);
+index = index +2 ;
+}
+
+let array = [ "apple" , "mango" , "orange"]
+let arr = 0 ;
+while (arr < array.length) {
+    console.log(`value is ${array[arr]}`);
+    arr++
+}
+
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++ ;
+    
+
+}while (score <= 10) {
+    
+}
