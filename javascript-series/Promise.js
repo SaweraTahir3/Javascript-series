9c03baf69e3d4bd6072e465c3d3c510450ea12e9
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-07-21 19:38:44 +0500

    working

--- javascript-series/Promise.js
+++ javascript-series/Promise.js
@@ -1,1 +1,29 @@
 //promise
+
+
+let promise = new Promise(function(resolve, reject) {
+  let foodReady = true;
+
+  if (foodReady) {
+    resolve("Food is delivered!");
+  } else {
+    reject("Food not delivered.");
+  }
+});
+
+promise
+  .then(function(result) {
+    console.log(result); // If success
+  })
+  .catch(function(error) {
+    console.log(error); // If error
+  });
+
+//Why Use Promises?
+// We use promises to handle asynchronous tasks, like:
+
+// Fetching data from a server
+
+// Reading files
+
+// Waiting for user input
