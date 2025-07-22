32886259226695fb16d7db51a91ba2d2f8f08e67
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-07-22 14:08:55 +0000

    work

--- javascript-series/Promise.js
+++ javascript-series/Promise.js
@@ -1,23 +1,42 @@
-//promise
+//promise 
 
 
-let promise = new Promise(function(resolve, reject) {
-  let foodReady = true;
+ const promiseOne = new Promise( function(resolve , reject){
+   setTimeout(function(){
+    console.log("asyn task one" );
+    resolve()
+   } , 1000)
+ }) // create this promise
 
-  if (foodReady) {
-    resolve("Food is delivered!");
-  } else {
-    reject("Food not delivered.");
-  }
-});
+   promiseOne.then(function(){
+    console.log('promise consumed');
+    
+   })// consumed promise
 
-promise
-  .then(function(result) {
-    console.log(result); // If success
-  })
-  .catch(function(error) {
-    console.log(error); // If error
-  });
+
+   new Promise(function(resolve, reject){
+    setTimeout(function(){
+      console.log('ayanc task two');
+      resolve()
+      
+    },1000)
+
+   }).then(function(){
+    console.log("aysanc 2 resolve");
+   })
+
+   const promiseThree = new Promise(function(resolve,reject){
+ setTimeout( function(){
+  resolve({username:"sawera" , email : "sawera@gmail.com"}) //resolve ki under jo parameter deyi
+  //  he wo save he next step  .then  me esaliy exsse kar sakhti he.
+ },1000)
+   })
+   promiseThree.then(function(user){
+console.log(user); // parameter ko exssise kar sakhti he yaha
+   })
+
+
+
 
 //Why Use Promises?
 // We use promises to handle asynchronous tasks, like:
