77956a949697f5faff2924890f7d50401ed97ab3
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-08-08 15:37:03 +0000

    work

--- javascript-series/Call.js
+++ javascript-series/Call.js
@@ -1,1 +1,18 @@
-work on tomorrow 
+// call method
+
+function setUserName(username){
+    this.username = username
+    console.log("called");
+
+}
+
+ function  createuser(username , email , password){
+    setUserName.call( this , username) // oper wali  this si yaha this me pass hua he username  phir add hua he object me 
+    this.email = email
+    this.password = password
+
+ }
+
+ const  called =  new createuser("sawera" , "developer@gmail.com" ,"codingLover")
+ console.log(called);
+ 
