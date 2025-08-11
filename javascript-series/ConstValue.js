09e903a0110c4b48856000a97fa9776efaf390ca
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-08-11 06:12:33 +0000

    w

--- javascript-series/ConstValue.js
+++ javascript-series/ConstValue.js
@@ -1,1 +1,32 @@
 // interview question
+
+// getOwnPropertyDescriptor  yai property kuch hide chezo ki bari me baa ti he 
+Object.getOwnPropertyDescriptor(Math , "PI")
+
+
+
+const mynewobj ={
+    name :"newObject" ,
+    isAvliable :"true" ,
+    feeback :"good",
+    ordernow : function(){
+        console.log('print hogaya');
+        
+    }
+
+}
+console.log(Object.getOwnPropertyDescriptor(mynewobj,  "name")); //check answer
+
+Object.defineProperty(mynewobj , "name", {
+ writable : false ,
+ enumerable : false
+})
+
+for (let [key , value] of Object.entries(mynewobj)) {
+ if(typeof value !== "function"){
+     console.log(`${key} , ${value}`);
+    
+ }
+    
+    
+}
